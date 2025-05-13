5488c0956cec06bda29b595296a780e2b480551c
washington-shoji <shoji.washington@gmail.com>
2025-05-12 20:00:00 -0500

    updated register page

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -2,6 +2,10 @@ import { ActionFunctionArgs, redirect } from '@remix-run/node';
 import { logout } from '~/services/auth.server';
 
 export async function action({ request }: ActionFunctionArgs) {
-	await logout(request);
-	return redirect('/login');
+	const destroyedSession = await logout(request);
+	return redirect('/login', {
+		headers: {
+			'Set-Cookie': destroyedSession,
+		},
+	});
 }
